894febaba1ff788f48d68e3fe75b15d39f25b662
corbs <corbsmartin@gmail.com>
2019-04-12 11:02:18 -0500

    make bake

--- src/main/java/io/corbs/WebUI.java
+++ src/main/java/io/corbs/WebUI.java
@@ -1,63 +1,24 @@
 package io.corbs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Map;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
-
-@Configuration
-class WebClientConfig {
-
-    @Bean
-    public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
-                               OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
-                clientRegistrationRepository, authorizedClientRepository);
-        return WebClient.builder()
-                .apply(oauth2.oauth2Configuration())
-                .build();
-    }
-}
 
 @Controller
 @SpringBootApplication
 public class WebUI {
-
-    @Autowired
-    private WebClient webClient;
 
     public static void main(String[] args) {
 		SpringApplication.run(WebUI.class, args);
 	}
 
     @GetMapping("/")
-    public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        model.addAttribute("userId", authorizedClient.getPrincipalName());
-        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-        String userInfoEndpointUri = authorizedClient.getClientRegistration()
-                .getProviderDetails().getUserInfoEndpoint().getUri();
-        Map userAttributes = this.webClient
-                .get()
-                .uri(userInfoEndpointUri)
-                .attributes(oauth2AuthorizedClient(authorizedClient))
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
-        model.addAttribute("userName", userAttributes.get("user_name").toString());
+    public String index(Model model) {
+        model.addAttribute("userId", "corbs");
+        model.addAttribute("clientName", "corbs");
+        model.addAttribute("userName", "corbs");
         return "index";
     }
 }
