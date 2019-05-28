6b1d33f2ca27691ee72393fbd7e47e418a46d029
corbs <corbsmartin@gmail.com>
2019-05-27 19:09:55 -0500

    scs integration

--- src/main/java/io/todos/WebUI.java
+++ src/main/java/io/todos/WebUI.java
@@ -3,11 +3,20 @@ package io.todos;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@EnableDiscoveryClient
+@EnableWebFluxSecurity
+@RefreshScope
 @SpringBootApplication
 public class WebUI {
 
@@ -23,6 +32,11 @@ public class WebUI {
         model.addAttribute("user", "Nacho Libre");
         model.addAttribute("placeholder", placeholder);
         return "index";
+    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange().anyExchange().permitAll().and().httpBasic().disable().csrf().disable().build();
     }
 }
 
