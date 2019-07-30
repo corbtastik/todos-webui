83a96d2cce8d45159403228396681ac145614aa3
corbs <corbsmartin@gmail.com>
2019-07-30 16:30:56 -0500

    adding detailed logging, including logging from spa

--- src/main/java/io/todos/WebUI.java
+++ src/main/java/io/todos/WebUI.java
@@ -18,9 +18,13 @@ public class WebUI {
 	@Value("${todos.webui.placeholder:cf push something?}")
 	private String placeholder;
 
+    @Value("${spring.security.user.name}")
+    private String username;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("placeholder", placeholder);
+        model.addAttribute("username", username);
         return "index";
     }
 }
